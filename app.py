c8ea7895ab6b3f3082641875968206ffe8f0d8c8
“Joey <joeylaufter@gmail.com>
2025-04-15 15:26:14 -0400

    Add /two route for secondary Flask endpoint

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/two')
+
+def hello2():
+
+    return "Hello again from Flask in Codespaces!"
+
+@app.route('/three')
+
+def hello2():
+
+    return "Hello again again from Flask in Codespaces!"
